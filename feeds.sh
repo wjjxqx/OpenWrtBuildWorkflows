cc815a9d3ee5e9a2b756a60d914461c3f3d91299
wjjxqx <37838883+wjjxqx@users.noreply.github.com>
2025-11-08 17:08:11 +0800

    Update feeds.sh

--- feeds.sh
+++ feeds.sh
@@ -39,3 +39,4 @@ echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git;master' >> f
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
+echo "src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git" >> "feeds.conf.default"
